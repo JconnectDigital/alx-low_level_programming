4898cb4507365f0095088b531db592a8c713a2a9
JconnectDigital <josephudoh999@gmail.com>
2022-06-05 13:56:26 -0700

    size 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+*/
+int main(void)
+{
+
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+	printf("size of a long int: %i byte(s)\n", sizeof(long));
+	printf("size of a long long int: byte(s)\n", sizeof(double));
+	printf("size of a float: byte(s)\n", sizeof(float));
+
+	return (0);
+
+}
