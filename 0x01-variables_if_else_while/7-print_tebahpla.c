2948ce7e22fb09df8c2302efde0b04b00e091a17
JconnectDigital <josephudoh999@gmail.com>
2022-06-05 17:56:00 -0700

    print alpha rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/** 
+ * main - print tebahpla.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char alpha_rev;
+
+	for (alpha_rev = 'z'; alpha_rev >= 'a'; alpha_rev--)
+	{
+		putchar(alpha_rev);
+	}
+
+	putchar('\n');
+	return (0);
+}
