f1008ec5c50bcd78f1cb0c5e16fe1fa21202bd47
JconnectDigital <josephudoh999@gmail.com>
2022-06-05 18:02:48 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers in base 16
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char b;
+
+	for (b = '0'; b <= '9'; b++)
+	{
+		putchar(b);
+	}
+
+	for (b = 'a'; b <= 'f'; b++)
+	{
+		putchar(b);
+	}
+
+	putchar('\n');
+	return (0);
+}
