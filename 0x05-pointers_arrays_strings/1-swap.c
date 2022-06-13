e3944119577aec14e1136caff776c7c390a7c9b7
JconnectDigital <josephudoh999@gmail.com>
2022-06-13 16:04:31 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - funtioncs that swaps 2 int
+ * @a: this is the first entry
+ * @b: this is the second entry
+ * Return: Void
+ */
+void swap_int(int *a, int *b)
+{
+
+	int changed;
+
+	changed = *a;
+	*a = *b;
+	*b = changed;
+}
