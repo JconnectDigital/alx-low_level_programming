8ff3ea9473c7546f4293b8a5db12199265a435ab
JconnectDigital <josephudoh999@gmail.com>
2022-06-13 16:21:44 -0700

    print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int index = 0;
+
+	for (index = 0; s[index] != '\0'; index++)
+		;
+
+	for (index--; index >= 0; index--)
+	{
+			_putchar(s[index]);
+	}
+	_putchar('\n');
+}
